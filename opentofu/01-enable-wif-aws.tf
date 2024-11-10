4b033be7109e79cfb67993664be6f54e61d5903c
Ben Dronen <dronenb@users.noreply.github.com>
2024-11-10 14:28:37 -0500

    wip
    Signed-off-by: Ben Dronen <dronenb@users.noreply.github.com>

--- opentofu/01-enable-wif-aws.tf
+++ opentofu/01-enable-wif-aws.tf
@@ -10,9 +10,8 @@ resource "aws_iam_openid_connect_provider" "minikube" {
   url = "https://storage.googleapis.com/dronenb-kubecon-2024-demo"
 
   client_id_list = [
-    "https://storage.googleapis.com/dronenb-kubecon-2024-demo",
     "sts.amazonaws.com" # Required audience for secrets store CSI driver: https://github.com/aws/secrets-store-csi-driver-provider-aws/blob/b8df4953967406978a6199ab5321ee2308a387a7/auth/auth.go#L32
   ]
 
-  thumbprint_list = ["cf23df2207d99a74fbe169e3eba035e633b65d94"]
+  thumbprint_list = ["c29664d13eb3ae9d78c3b227ac1eda0efba455b9"]
 }

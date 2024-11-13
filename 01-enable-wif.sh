a412bbe0cb58b869084323dafe01cebf79c36e79
Ben Dronen <dronenb@users.noreply.github.com>
2024-11-13 07:09:23 -0700

    wip
    Signed-off-by: Ben Dronen <dronenb@users.noreply.github.com>

--- 01-enable-wif.sh
+++ 01-enable-wif.sh
@@ -11,6 +11,10 @@ pe "bat --paging=never  opentofu/01-enable-wif.tf"
 pe "minikube start 
     --extra-config=apiserver.service-account-issuer=https://storage.googleapis.com/dronenb-kubecon-2024-demo
     --extra-config=apiserver.service-account-jwks-uri=https://storage.googleapis.com/dronenb-kubecon-2024-demo/openid/v1/jwks"
+# I have seen this happen:
+# error: failed to create token: serviceaccounts "default" not found
+# Sleep for a few seconds so it doesn't
+sleep 2
 pe "kubectl create token --namespace default default | jc --jwt -p"
 pe "kubectl get --raw /.well-known/openid-configuration | gcloud storage cp --cache-control=no-cache /dev/stdin gs://dronenb-kubecon-2024-demo/.well-known/openid-configuration"
 pe "kubectl get --raw /openid/v1/jwks | gcloud storage cp --cache-control=no-cache /dev/stdin gs://dronenb-kubecon-2024-demo/openid/v1/jwks"
--- 02-secrets-store-csi-driver.sh
+++ 02-secrets-store-csi-driver.sh
@@ -6,8 +6,15 @@ set -o pipefail
 shopt -s nullglob
 
 source ./demo-magic.sh
-pe "bat --paging=never  opentofu/02-*"
+pe "bat --paging=never opentofu/02-*"
 cd kubernetes/02-secrets-store-csi-driver/manifests/overlays/minikube
+# Do this under the hood so CRD's get applied ahead of time and we don't see an error
+kubectl apply -f ../../base/customresourcedefinition.yaml > /dev/null
 pe "kubectl kustomize | kubectl apply -f -"
-pe "kubectl wait --for=condition=Ready -n default pod/secrets-demo"
-pe "kubectl exec -it pod/secrets-demo  -- cat "
+pe "bat --paging=never secretproviderclass.yaml"
+pe "bat --paging=never pod.yaml"
+pe "kubectl wait pod --all --for=condition=Ready --namespace=secrets-store-csi-driver --timeout=-1s"
+pe "kubectl apply -f pod.yaml"
+pe "kubectl wait --for=condition=Ready -n default pod/secrets-demo --timeout=40s"
+pe 'kubectl exec -it pod/secrets-demo  -- /bin/bash -c "ls /var/run/secrets/awssecrets /var/run/secrets/azuresecrets /var/run/secrets/gcpsecrets"'
+pe 'kubectl exec -it pod/secrets-demo  -- /bin/bash -c "cat /var/run/secrets/awssecrets/arn:aws:secretsmanager:us-east-1:726600616558:secret:example-aws-secret-5L2mYm; echo; cat /var/run/secrets/azuresecrets/example-azure-secret; echo; cat /var/run/secrets/gcpsecrets/example-gcp-secret"'
